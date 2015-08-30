96f14e57af2f33478f19bbad415caf122f2ecf71
mmayfield1 <mark@kandm-solutions.com>
2015-08-30 16:15:32 -0500

    completed Windows jphs integration

--- jphs.py
+++ jphs.py
@@ -8,6 +8,8 @@ arch = str(8 * struct.calcsize("P"))
 class jphs:
 
 	def jphideit2(self, widget):
+		cmd = "export WINEDEBUG=warn-all,-heap,-relay,err-all,fixme-all,trace-all"
+		os.system(cmd)
 		self.file = self.builder.get_object("entry1")
 		self.sfile = self.file.get_text()
 		self.password = self.builder.get_object("entry5")
@@ -15,18 +17,25 @@ class jphs:
 		self.fchooser = self.builder.get_object("filechooserbutton1")
 		self.hidefile = self.fchooser.get_filename()
 		self.buffer1 = self.builder.get_object("textbuffer3")
+		checkwin = self.builder.get_object("checkbutton18")
 		self.fileinfo = self.builder.get_object("entry3")
 		filetype = self.fileinfo.get_text()
 		if self.sfile != '' and str.strip(self.spass) != '' and self.hidefile != None:
 			if "JPEG" in filetype:
 				head, tail = os.path.split(self.sfile)
-				outdir = home + tail + '/jphide'
+				needwin = ("OFF", "ON")[checkwin.get_active()]
+				if needwin == "ON":
+					outdir = home + tail + '/jphidewin'
+					progcmd = "/usr/bin/wine " + execdir + "/programs/Win/jphide.exe " 
+				else:
+					outdir = home + tail + '/jphidelin'
+					progcmd = execdir + "/programs/" + arch + "/jphide "
 				if not os.path.isdir(outdir):
 					os.mkdir(outdir)
 				self.outfile = outdir + '/' +tail
 				if os.path.isfile(self.outfile):
 					os.remove(self.outfile)
-				cmd = execdir + "/programs/" + arch + "/jphide " + re.escape(self.sfile) + " " + re.escape(self.outfile) + " " + re.escape(self.hidefile)
+				cmd = progcmd + re.escape(self.sfile) + " " + re.escape(self.outfile) + " " + re.escape(self.hidefile)
 				child = pexpect.spawn(cmd)
 				child.expect('Passphrase:', timeout=2)
 				child.sendline(self.spass)
@@ -43,23 +52,32 @@ class jphs:
 			self.showdiag()
 
 	def jpseekit2(self, widget):
+		cmd = "export WINEDEBUG=warn-all,-heap,-relay,err-all,fixme-all,trace-all"
+		os.system(cmd)
 		self.file = self.builder.get_object("entry1")
 		self.sfile = self.file.get_text()
 		self.password = self.builder.get_object("entry4")
 		self.spass = self.password.get_text()
 		self.buffer1 = self.builder.get_object("textbuffer3")
 		self.fileinfo = self.builder.get_object("entry3")
+		checkwin = self.builder.get_object("checkbutton19")
 		filetype = self.fileinfo.get_text()
 		if self.sfile != '' and self.spass != '':
 			if "JPEG" in filetype:
 				head, tail = os.path.split(self.sfile)
-				outdir = home + tail + '/jpseek'
+				needwin = ("OFF", "ON")[checkwin.get_active()]
+				if needwin == "ON":
+					outdir = home + tail + '/jpseekwin'
+					progcmd = "/usr/bin/wine " + execdir + "/programs/Win/jpseek.exe " 
+				else:
+					outdir = home + tail + '/jpseeklin'
+					progcmd = execdir + "/programs/" + arch + "/jpseek "
 				if not os.path.isdir(outdir):
 					os.mkdir(outdir)
 				self.outfile = outdir + '/' + tail + '.txt'
 				if os.path.isfile(self.outfile):
 					os.remove(self.outfile)
-				cmd = execdir + "/programs/" + arch + "/jpseek " + re.escape(self.sfile) + " " + re.escape(self.outfile)
+				cmd = progcmd + re.escape(self.sfile) + " " + re.escape(self.outfile)
 				child = pexpect.spawn(cmd)
 				child.expect('Passphrase:', timeout=2)
 				child.sendline(self.spass)
@@ -73,3 +91,4 @@ class jphs:
 		else:
 			self.buffer1.set_text("You must select a valid file, and insert a password")
 			self.showdiag()
+
