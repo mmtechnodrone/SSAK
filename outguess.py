ba6a8a589fa29492e609ab417091b5a6291ca2c5
mmayfield1 <mark@kandm-solutions.com>
2015-10-18 19:25:50 -0500

    Modified Outguess GUI, added checks for Outguess version and password.

--- outguess.py
+++ outguess.py
@@ -13,10 +13,12 @@ class outguess:
 		self.file = self.builder.get_object("entry1")
 		self.sfile = self.file.get_text()
 
-
 	def extractguess(self, widget):
 		self.ogpass2 = self.builder.get_object("entry11")
 		extractpassbox = self.builder.get_object("checkbutton24")
+		out1 = ("OFF", "ON")[extractpassbox.get_active()]
+		outguessver = self.builder.get_object("checkbutton22")
+		out2 = ("OFF", "ON")[outguessver.get_active()]
 		pass2 = self.ogpass2.get_text()
 		self.file = self.builder.get_object("entry1")
 		self.sfile = self.file.get_text()
@@ -24,15 +26,31 @@ class outguess:
 		self.info = self.fileinfo.get_text()
 		self.buffer1 = self.builder.get_object("textbuffer3")
 		head, tail = os.path.split(self.sfile)
+		cmd = ''
 		outdir = home + tail + '/outguessextract'
+		if out2 == "ON":
+			prog = re.escape(execdir) + "/programs/" + arch + "/outguess_0.13"
+		else:
+			prog = re.escape(execdir) + "/programs/" + arch + "/outguess_0.2"
 		if not os.path.isdir(outdir):
 			os.mkdir(outdir)
 		if self.sfile != '':
 			if 'JPEG' or 'JPG' or 'PNM' or 'PPM' in self.info:
-				cmd = re.escape(execdir) + "/programs/" + arch + "/outguess_0.13 -r -k " + re.escape(pass2) + " " + re.escape(self.sfile) + " " + re.escape(outdir) + "/outguessoutput"
-				proc = Popen(cmd, shell = True,stdout=PIPE)
-				self.buffer1.set_text("Output should be at " + outdir + "/outguessoutput")
-				self.showdiag()
+				if out1 == "ON":
+					if str.strip(pass2) == '':	
+						self.buffer1.set_text("You must enter a password if the password box is checked!")	
+						self.showdiag()		
+					else:
+						cmd = prog + " -r -k " + re.escape(pass2) + " " + re.escape(self.sfile) + " " + re.escape(outdir) + "/outguessoutput"
+						proc = Popen(cmd, shell = True,stdout=PIPE)
+						self.buffer1.set_text("Output should be at " + outdir + "/outguessoutput")
+						self.showdiag()
+				else:
+					cmd = prog + " -r " + re.escape(self.sfile) + " " + re.escape(outdir) + "/outguessoutput"
+					print cmd
+					proc = Popen(cmd, shell = True,stdout=PIPE)
+					self.buffer1.set_text("Output should be at " + outdir + "/outguessoutput")
+					self.showdiag()
 		else:
 			self.buffer1.set_text("Please select a file from the file menu!")	
 			self.showdiag()
